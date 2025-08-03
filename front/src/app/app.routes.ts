a5692880c2dcfc33c77677ce5e50241b596cdadb
Dissoares <diegosoaresyt@gmail.com>
2025-08-03 20:03:11 -0300

    configurando rotas

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -1,3 +1,18 @@
+import { RotasEnum } from './core/enums/rotas.enum';
 import { Routes } from '@angular/router';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+  {
+    path: RotasEnum.ROOT,
+    loadComponent: () =>
+      import('./layout/conteudo/conteudo.component').then(
+        (m) => m.ConteudoComponent
+      ),
+  },
+
+  {
+    path: '**',
+    redirectTo: RotasEnum.ESTABELECIMENTO.CATALOGO,
+    pathMatch: 'full',
+  },
+];
