7a59fd3fb24a8c29be46066af9720d12232ac199
Diego da Silva Soares <diegosoaresyt@gmail.com>
2025-08-04 09:09:08 -0300

    adicionando back em Spring WebApi

---
+++ back/src/main/java/br/com/gestack/domain/GeStackApplication.java
@@ -0,0 +1,13 @@
+package br.com.gestack.domain;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GeStackApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GeStackApplication.class, args);
+	}
+
+}
