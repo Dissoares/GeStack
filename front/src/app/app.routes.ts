88db7108120e297633a2663d73d45abd376bcbea
Dissoares <diegosoaresyt@gmail.com>
2025-08-04 23:25:35 -0300

    configurando rotas

--- front/src/app/app.routes.ts
+++ front/src/app/app.routes.ts
@@ -2,14 +2,82 @@ import { Routes } from '@angular/router';
 
 export const routes: Routes = [
   {
-    path: '',
+    path: 'home',
     loadComponent: () =>
       import('./auth/auth.component').then((m) => m.AuthComponent),
+  },
+  {
+    path: '',
+    loadComponent: () =>
+      import('./layout/conteudo/conteudo.component').then(
+        (c) => c.ConteudoComponent
+      ),
+    children: [
+      {
+        path: 'administrador',
+        children: [
+          {
+            path: 'listagem',
+            loadComponent: () =>
+              import(
+                './modules/administrador/administrador-listagem/administrador-listagem.component'
+              ).then((c) => c.AdministradorListagemComponent),
+          },
+          {
+            path: 'formulario',
+            loadComponent: () =>
+              import(
+                './modules/administrador/administrador-formulario/administrador-formulario.component'
+              ).then((c) => c.AdministradorFormularioComponent),
+          },
+        ],
+      },
+
+      {
+        path: 'lider',
+        children: [
+          {
+            path: 'listagem',
+            loadComponent: () =>
+              import(
+                './modules/lideranca/lideranca-listagem/lideranca-listagem.component'
+              ).then((c) => c.LiderancaListagemComponent),
+          },
+          {
+            path: 'formulario',
+            loadComponent: () =>
+              import(
+                './modules/lideranca/lideranca-formulario/lideranca-formulario.component'
+              ).then((c) => c.LiderancaFormularioComponent),
+          },
+        ],
+      },
+
+      {
+        path: 'squad',
+        children: [
+          {
+            path: 'listagem',
+            loadComponent: () =>
+              import(
+                './modules/membros-squad/membro-squad-listagem/membro-squad-listagem.component'
+              ).then((c) => c.MembroSquadListagemComponent),
+          },
+          {
+            path: 'formulario',
+            loadComponent: () =>
+              import(
+                './modules/membros-squad/membro-squad-formulario/membro-squad-formulario.component'
+              ).then((c) => c.MembroSquadFormularioComponent),
+          },
+        ],
+      },
+    ],
   },
 
   {
     path: '**',
-    redirectTo: '',
+    redirectTo: 'home',
     pathMatch: 'full',
   },
 ];
