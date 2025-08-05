c872b4d5413af01afb5c029f13a48f7e4ce46dea
Dissoares <diegosoaresyt@gmail.com>
2025-08-04 22:49:03 -0300

    adicionando componente membros das squads

---
+++ front/src/app/modules/membros-squad/membro-squad-listagem/membro-squad-listagem.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-membro-squad-listagem',
+  templateUrl: './membro-squad-listagem.component.html',
+  styleUrls: ['./membro-squad-listagem.component.scss'],
+})
+export class MembroSquadListagemComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
