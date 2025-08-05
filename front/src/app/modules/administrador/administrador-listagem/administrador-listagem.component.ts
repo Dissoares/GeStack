3975cf4b183f31bc8923fbb2c679f42946146763
Dissoares <diegosoaresyt@gmail.com>
2025-08-04 22:48:26 -0300

    adicionando componente adminstrador

---
+++ front/src/app/modules/administrador/administrador-listagem/administrador-listagem.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-administrador-listagem',
+  templateUrl: './administrador-listagem.component.html',
+  styleUrls: ['./administrador-listagem.component.scss'],
+})
+export class AdministradorListagemComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
