7df1c1d7b060931bb3464a90703bf0422deb3e67
Dissoares <diegosoaresyt@gmail.com>
2025-08-04 22:48:47 -0300

    adicionando componentes lideranca

---
+++ front/src/app/modules/lideranca/lideranca-formulario/lideranca-formulario.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lideranca-formulario',
+  templateUrl: './lideranca-formulario.component.html',
+  styleUrls: ['./lideranca-formulario.component.scss'],
+})
+export class LiderancaFormularioComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
---
+++ front/src/app/modules/lideranca/lideranca-listagem/lideranca-listagem.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-lideranca-listagem',
+  templateUrl: './lideranca-listagem.component.html',
+  styleUrls: ['./lideranca-listagem.component.scss'],
+})
+export class LiderancaListagemComponent implements OnInit {
+  constructor() {}
+
+  ngOnInit() {}
+}
