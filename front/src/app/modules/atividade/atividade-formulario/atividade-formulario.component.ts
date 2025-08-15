0574d7203c71d746bb772ec08d394feb3e1fcc9d
Dissoares <diegosoaresyt@gmail.com>
2025-08-14 23:17:17 -0300

    adicionando modulo de atividades

---
+++ front/src/app/modules/atividade/atividade-formulario/atividade-formulario.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-atividade-formulario',
+  standalone: true,
+  imports: [],
+  templateUrl: './atividade-formulario.component.html',
+  styleUrls: ['./atividade-formulario.component.scss'],
+})
+export class AtividadeFormularioComponent implements OnInit {
+  constructor() {}
+
+  public ngOnInit() {}
+}
---
+++ front/src/app/modules/atividade/atividade-listagem/atividade-listagem.component.ts
@@ -0,0 +1,14 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-atividade-listagem',
+  standalone: true,
+  imports: [],
+  templateUrl: './atividade-listagem.component.html',
+  styleUrls: ['./atividade-listagem.component.scss'],
+})
+export class AtividadeListagemComponent implements OnInit {
+  constructor() {}
+
+  public ngOnInit() {}
+}
