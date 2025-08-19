187347a1657fd85a4c079a319df9719681b9f40f
Dissoares <diegosoaresyt@gmail.com>
2025-08-18 22:49:38 -0300

    validando ao salvar skill

--- back/src/main/java/br/com/gestack/domain/controlers/SkillController.java
+++ back/src/main/java/br/com/gestack/domain/controlers/SkillController.java
@@ -1,17 +1,16 @@
 package br.com.gestack.domain.controlers;
 
-import org.springframework.security.core.annotation.AuthenticationPrincipal;
-import org.springframework.security.core.userdetails.UserDetails;
 import br.com.gestack.domain.repository.UsuarioRepository;
 import org.springframework.security.core.Authentication;
-import br.com.gestack.domain.dtos.ListagemUsuariosDTO;
 import br.com.gestack.domain.services.SkillService;
 import org.springframework.web.bind.annotation.*;
 import org.springframework.http.ResponseEntity;
 import br.com.gestack.domain.businnes.Usuario;
 import br.com.gestack.domain.businnes.Skill;
+import org.springframework.http.HttpStatus;
 import lombok.AllArgsConstructor;
 import java.util.List;
+import java.util.Map;
 
 @AllArgsConstructor
 @RestController
@@ -21,8 +20,13 @@ public class SkillController {
     private final UsuarioRepository usuarioRepository;
 
     @PostMapping("/cadastrar")
-    public Skill cadastrar(@RequestBody Skill skill) {
-        return skillService.cadastrar(skill);
+    public ResponseEntity<Skill> cadastrar(@RequestBody Skill skill) {
+        try {
+            skillService.cadastrar(skill);
+            return ResponseEntity.status(HttpStatus.CREATED).build();
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
     }
 
     @GetMapping("/buscarTudo")
@@ -37,14 +41,17 @@ public class SkillController {
     }
 
     @PutMapping("/atualizar")
-    public ResponseEntity<Skill> atualizar(@RequestBody Skill skill,  Authentication auth) {
+    public ResponseEntity<Skill> atualizar(@RequestBody Skill skill, Authentication auth) {
         Usuario usuarioLogado = (Usuario) auth.getPrincipal();
-
-        Usuario usuario = usuarioRepository.findById(usuarioLogado.getIdUsuario())
-                .orElseThrow(() -> new RuntimeException("Usuário logado não encontrado"));
-
+        Usuario usuario = usuarioRepository.findById(usuarioLogado.getIdUsuario()).orElseThrow(() -> new RuntimeException("Usuário logado não encontrado"));
         skill.setModificadoPor(usuario);
-        return ResponseEntity.ok(skillService.atualizar(skill));
+
+        try {
+            Skill atualizado = skillService.atualizar(skill);
+            return ResponseEntity.ok(atualizado);
+        } catch (RuntimeException e) {
+            return ResponseEntity.status(HttpStatus.CONFLICT).build();
+        }
     }
 
 }
